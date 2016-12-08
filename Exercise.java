59ab96fa17c7b308bb566fa816be6b3608e32c22
clbokea <clbo@kea.dk>
2016-12-08 14:21:30 +0100

    initial

---
+++ Exercise.java
@@ -0,0 +1,14 @@
+/**
+ In the code below there will be 5 errors.    
+Your job is to identify where these errors are, and to fix them. A tip is to start out by pressing the debug *"+"* botton and look in the output pane. 
+Here you will get a description about what is wrong in the code.
+
+Like always: Fork this repo, and create a pull request when you are finnished.
+ */
+public class Exercise 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World");
+    }
+}
---
+++ exe_5_err.md
@@ -0,0 +1,10 @@
+# Find 5 Errors
+
+In the code we provide in [exercise.java](/exercise.java) there will be 5 errors.    
+Your job is to identify where these errors are, and to fix them. A tip is to start out by pressing the debug *"+"* botton and look in the output pane. 
+Here you will get a description about what is wrong in the code.
+
+Like always: Fork this repo, and create a pull request when you are finnished.
+
+
+&copy; _clbo@kea.dk 8-12-2016_
